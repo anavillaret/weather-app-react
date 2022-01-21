7feb4ab61c4aa630a62e90d23982ed961627aebe
anavillaret <93731980+anavillaret@users.noreply.github.com>
2022-01-21 15:15:32 +0000

    Inserted logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Weather from "./Weather";
+import Logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
       <h1 classNamea="app-title" alt="logo" width="10rem">
+        <img src={Logo} alt="logo" width="160px" />
         <span className="title">WEATHER </span>
       </h1>
       <Weather />

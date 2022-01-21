b587d10ccee0b5d1203dfac8c94a947b884521f1
anavillaret <93731980+anavillaret@users.noreply.github.com>
2022-01-21 12:01:15 +0000

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,11 @@
 import React from "react";
-import axios from "axios";
+
 import { Rings } from "react-loader-spinner";
 
 export default function Weather() {
   return (
     <div className="search">
-      Ready to search
+      Ready to build my app
       <Rings color="#00BFFF" height={80} width={80} />
     </div>
   );
